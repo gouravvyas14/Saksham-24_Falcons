d15da6cd32d4385e855214942209b4c6bc9ee105
gouravvyas14 <gouravvyas510@gmail.com>
2024-06-21 00:44:56 +0530

    Create builtinLib.py

---
+++ week 2/day2/builtinLib.py
@@ -0,0 +1,30 @@
+import math
+
+# Square root
+x = 25
+print(math.sqrt(x)) 
+
+# Exponentiation (e^x)
+y = 2.5
+print(math.exp(y)) 
+
+# Logarithms
+z = 100
+print(math.log(z, 10)) 
+
+# Trigonometric functions (radians)
+angle_rad = math.radians(45)
+print(math.sin(angle_rad))
+
+import datetime 
+date_today = datetime.date.today() 
+time_now = datetime.datetime.now().time() 
+print(date_today) 
+print(time_now)
+
+import random
+# Generate a random float between 0 and 1
+print(random.random()) 
+
+# Generate a random integer between a specified range
+print(random.randint(1, 10))
